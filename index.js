51c37d3765a0bb84e3f67d009fb32dc06e77fe60
SugBrar <brar.sugreev@gmail.com>
2022-10-31 12:09:41 -0400

    declaring constants

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // this is my first javascript code
 console.log('Hello World');
+
 //variables
 let name ='Gill';
 console.log(name);
+
+//constants
+const intrestRate =0.3;
+//intrestRate =1;
+console.log(intrestRate);
