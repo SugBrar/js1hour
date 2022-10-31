dd4ce94ceaf6ce342aba2b81314059487f8ec5d3
SugBrar <brar.sugreev@gmail.com>
2022-10-31 15:51:26 -0400

    Primitive data types

--- JavaScript/variable.js
+++ JavaScript/variable.js
@@ -15,3 +15,23 @@ const z= 149;
 console.log(z);
 //z= 900;  because z is constant and cannot be changed
 console.log(z);
+
+
+// Primitive datatypes 
+let number = 190;
+console.log(typeof(number)); // number
+
+let price = 30.52;
+console.log(typeof(price)); // number
+
+let stuName = 'John';
+console.log(typeof(stuName)); // string
+
+let flag = false;
+console.log(typeof(flag)); // boolean
+
+let value;
+console.log(typeof(value)); //undefined
+
+let status = null;
+console.log(typeof(status)); // null as object
