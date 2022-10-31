75f2e45791f11810fc421deb909e4a27112b111f
SugBrar <brar.sugreev@gmail.com>
2022-10-31 16:06:21 -0400

    Operators concept

---
+++ JavaScript/operatorsdemo.js
@@ -0,0 +1,11 @@
+let x= 10, y = 20;
+
+// Airthmetic operators
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x**y);
+console.log(x/y);
+console.log(y%x);
+console.log(x++);
+console.log(y--);
