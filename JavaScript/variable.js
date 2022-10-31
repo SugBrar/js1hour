20c4a10a7ab5bfa95b8e09092a32bd388da1934b
SugBrar <brar.sugreev@gmail.com>
2022-10-31 15:39:21 -0400

    let concept

--- JavaScript/variable.js
+++ JavaScript/variable.js
@@ -1,4 +1,11 @@
-
+// variables - 1. var (function scope)
 var x ="Hello", y= 150;
 
 document.write(x);
+console.log(y);
+
+// 2. let (block scope)
+let text;
+text = 'Welcome';
+
+console.log(text);
