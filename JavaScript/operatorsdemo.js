4dc727f24d3731fccaab37d5316881ed213ce3fb
SugBrar <brar.sugreev@gmail.com>
2022-10-31 16:22:36 -0400

    different operators concept

--- JavaScript/operatorsdemo.js
+++ JavaScript/operatorsdemo.js
@@ -1,11 +1,42 @@
-let x= 10, y = 20;
+//let x= 20, y = 5 ;
 
-// Airthmetic operators
-console.log(x+y);
-console.log(x-y);
-console.log(x*y);
-console.log(x**y);
-console.log(x/y);
-console.log(y%x);
-console.log(x++);
-console.log(y--);
+// // Airthmetic operators
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x**y);
+// console.log(x/y);
+// console.log(y%x);
+// console.log(x++);
+// console.log(--x);
+// console.log(++y);
+// console.log(y--);
+
+// //Assignment 
+
+// console.log(x+=y);
+// console.log(x-=y);
+// console.log(x*=y);
+// console.log(x/=y);
+// console.log(y%=x);
+
+// // Relational or Comparison operators - always return a boolean value (true/false)
+// let x = 2, y = 1;
+
+// console.log(x==y);
+// console.log(x>y);
+// console.log(x<=y);
+// console.log(x!=y);
+
+// // termnary operator
+// console.log(x<y ? x:y); // return value of y 1
+// console.log(x>y ? x:y); // return value of x 2
+
+// Logical operators
+
+let a = false;
+let b = false;
+
+console.log(a && b);
+console.log(a || b); // return true if one of them is true
+console.log (!b); // compliment b
