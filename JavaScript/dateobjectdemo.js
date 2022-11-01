fba66ecddc6a6907815e99611aedeb9ad4a18216
SugBrar <brar.sugreev@gmail.com>
2022-11-01 15:02:01 -0400

    Date object basic

---
+++ JavaScript/dateobjectdemo.js
@@ -0,0 +1,14 @@
+
+let d = new Date();
+
+// console.log(d);
+// console.log(d.getDate()); //1-31
+// console.log(d.getMonth()); //0-11
+//to get exact month
+// console.log(d.getMonth()+1); //1-12
+// console.log(d.getFullYear());
+
+console.log(d.getDate()+"-"+(d.getMonth()+1)+"-"+d.getFullYear());
+
+console.log(d.getHours()+":"+d.getMinutes()+":"+d.getSeconds());
+
