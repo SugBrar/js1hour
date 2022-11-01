f04200b78cd7272e59e4035f2de58b7e748ad3a2
SugBrar <brar.sugreev@gmail.com>
2022-11-01 10:16:08 -0400

    break and continue concept

---
+++ JavaScript/jumpingStatements.js
@@ -0,0 +1,25 @@
+//break
+
+for(let i=1;i<=10;i++)
+{
+    if(i==5)
+    break;
+    console.log(i);
+}
+console.log("Loop Exit");
+
+
+//continue - skipping the value
+for(let j=1;j<=10;j++)
+{
+    if(j==5)
+    continue;
+    console.log(j);
+}
+
+for (k=1;k<=10;k++)
+{
+    if(k==3 || k==6 )
+    continue;
+    console.log(k);
+}
