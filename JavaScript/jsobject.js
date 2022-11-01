ed5e7f84c68295a22ea3fb3cebd678ae78ff4f0b
SugBrar <brar.sugreev@gmail.com>
2022-11-01 11:26:24 -0400

    for in loop concept

--- JavaScript/jsobject.js
+++ JavaScript/jsobject.js
@@ -5,22 +5,29 @@ age: 45,
 weight: 67
 }
 
-// Accessing object properties
+// // Accessing object properties
 
-console.log(person["firstName"]);
-console.log(person.age);
+// console.log(person["firstName"]);
+// console.log(person.age);
 
-// add new property to the existing object e.g add height 
-//person["height"] =5.6;
-person.height =5.8;
-console.log(person.height);
+// // add new property to the existing object e.g add height 
+// //person["height"] =5.6;
+// person.height =5.8;
+// console.log(person.height);
 
 
-// update the existing propertyof the object
-person.weight = 75;
-console.log(person.weight);
+// // update the existing propertyof the object
+// person.weight = 75;
+// console.log(person.weight);
 
-//Remove the propeerty from the object
-delete person.age;
-console.log(person.age);
+// //Remove the propeerty from the object
+// delete person.age;
+// console.log(person.age);
 
+// for in loop in objects
+for(let x in person) 
+{
+    //console.log(x); // print only name of the property of the object
+    //console.log(person[x]); // print only value of the property of the object
+    console.log(x+": "+person[x]); // print property name and corresding values of the object
+}
