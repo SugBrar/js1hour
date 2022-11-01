dcd3325908aa539dbc1b3d14f1839f7190872798
SugBrar <brar.sugreev@gmail.com>
2022-11-01 10:31:43 -0400

    function concept

---
+++ JavaScript/jsfunction.js
@@ -0,0 +1,24 @@
+function add(a,b)
+{
+    console.log(a+b);
+}
+add(19,20);
+
+function addd(c,d)
+{
+    return(c+d);
+}
+let res = addd(10,20);
+console.log(res);
+
+
+
+function greetings(){
+    console.log("Hey,Good Morning");
+}
+greetings();
+
+function greetingss(){
+    return("Hey, Good Morning!");
+}
+console.log(greetingss());
