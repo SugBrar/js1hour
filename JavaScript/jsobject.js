6764b740537ab717b248a2301a2d4f540e7967c0
SugBrar <brar.sugreev@gmail.com>
2022-11-01 11:20:02 -0400

    different properties of the object

--- JavaScript/jsobject.js
+++ JavaScript/jsobject.js
@@ -10,3 +10,17 @@ weight: 67
 console.log(person["firstName"]);
 console.log(person.age);
 
+// add new property to the existing object e.g add height 
+//person["height"] =5.6;
+person.height =5.8;
+console.log(person.height);
+
+
+// update the existing propertyof the object
+person.weight = 75;
+console.log(person.weight);
+
+//Remove the propeerty from the object
+delete person.age;
+console.log(person.age);
+
