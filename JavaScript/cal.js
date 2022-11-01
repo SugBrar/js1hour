63cf823fec906c44d3575828b770485abda0f397
SugBrar <brar.sugreev@gmail.com>
2022-11-01 11:05:25 -0400

    function practise

--- JavaScript/cal.js
+++ JavaScript/cal.js
@@ -1,2 +1,25 @@
 
-document.getElementById("val1")
+document.getElementById("addbtn").onclick = add2Numbers;
+document.getElementById("subbtn").onclick = sub2Numbers;
+document.getElementById("mulbtn").onclick = mul2Numbers;
+
+function add2Numbers(){
+    let a = document.getElementById("val1").value;
+    let b = document.getElementById("val2").value;
+    let res = parseInt(a) + parseInt(b);
+    document.getElementById("Result").innerHTML=res;
+}
+
+function sub2Numbers(){
+    let a = document.getElementById("val1").value;
+    let b = document.getElementById("val2").value;
+    let res = parseInt(a) - parseInt(b);
+    document.getElementById("Result").innerHTML=res;
+}
+
+function mul2Numbers(){
+    let a = document.getElementById("val1").value;
+    let b = document.getElementById("val2").value;
+    let res = parseInt(a) * parseInt(b);
+    document.getElementById("Result").innerHTML=res;
+}
