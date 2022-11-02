3b51381a8bbb76cabfd885b57e52ee30082e9dd6
SugBrar <brar.sugreev@gmail.com>
2022-11-02 12:16:57 -0400

    Multilevel inheritance concept

--- JavaScript/inheritance.js
+++ JavaScript/inheritance.js
@@ -14,13 +14,26 @@ class B extends A{
     }
 }
 
+//Grandchild Class
+class C extends B{
+    c = 70;
+    view(){
+        console.log(this.c);
+    }
+}
 
 objA = new A();
-console.log("Parent class")
+console.log("Parent class");
 objA.display();
 
 
 objB = new B();
-console.log("Child Class")
+console.log("Child Class");
 objB.display();
 objB.show();
+
+objC = new C();
+console.log("GranChild");
+objC.display();
+objC.show();
+objC.view();
