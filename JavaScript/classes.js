1c1e725bb198cba65a516b5607f6245adbfa63a1
SugBrar <brar.sugreev@gmail.com>
2022-11-02 10:50:17 -0400

    Multiple objects of a Class

--- JavaScript/classes.js
+++ JavaScript/classes.js
@@ -21,3 +21,11 @@ class student{
 let stu = new student(35,"John","A");
 //stu.setDetails(21,"John","A");
 stu.display();
+
+//mutiple objects - each object is independent
+let stu1 = new student(12,"David","AB");
+stu1.display();
+let stu2 = new student(21,"David","C");
+stu2.display();
+let stu3 = new student(112,"Smith","B");
+stu3.display();
