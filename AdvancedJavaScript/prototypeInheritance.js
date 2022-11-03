d8d1409ad7b9a201a232f6021c6adcb6c752dd6a
SugBrar <brar.sugreev@gmail.com>
2022-11-03 14:25:39 -0400

    second function using first function's prototype

---
+++ AdvancedJavaScript/prototypeInheritance.js
@@ -0,0 +1,23 @@
+function Person(fName,lName){
+    this.firstName = fName
+    this.lastName = lName
+}
+
+Person.prototype.getFullName = function(){
+    return this.firstName + ' ' + this.lastName
+}
+
+function SuperHero(fName,lName){
+    Person.call(this,fName,lName)
+    this.isSuperHero = true
+}
+
+SuperHero.prototype.fightCrime = function(){
+    console.log('Fighting Crime')
+}
+
+SuperHero.prototype = Object.create(Person.prototype) // SuperHero will call getFullName method through object-Person
+
+const batman = new SuperHero('Peter','Wayne')
+SuperHero.prototype.construtor = SuperHero
+console.log(batman.getFullName())
