69a9697c00daee2349782d5cd0c46fbb666c5bc5
SugBrar <brar.sugreev@gmail.com>
2022-11-03 14:36:56 -0400

    class, inheritance, super keyword

---
+++ AdvancedJavaScript/classInheritance.js
@@ -0,0 +1,26 @@
+class Person{
+    constructor(fName,lName){
+        this.firstName = fName
+        this.lastName = lName
+    }
+    sayName(){
+        return this.firstName + ' ' + this.lastName
+    }
+}
+
+const p1 = new Person('Bruce','Wayne') 
+console.log(p1.sayName())
+
+class SuperHero extends Person{
+    constructor(fName,lName){
+        super(fName,lName)
+        this.isSuperHero = true
+    }
+
+    fightCrime(){
+        console.log('Fighting crime')
+    }
+}
+
+const batman = new SuperHero('Bruce','Wayne')
+console.log(batman.sayName())
