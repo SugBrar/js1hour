95383de317e772a58a25c694a04e188be9952471
SugBrar <brar.sugreev@gmail.com>
2022-11-03 09:20:18 -0400

    convevrt JSON into JS

---
+++ JavaScript/jasonToJSObject.js
@@ -0,0 +1,9 @@
+let text ='{"employees" : [ ' +
+'{ "firstName": "John" , "lastName" : "Doe"},'+
+'{ "firstName": "Anna" , "lastName" : "Smith"},'+
+'{ "firstName": "Peter" , "lastName" : "Jones"} ]}';
+
+let obj = JSON.parse(text);
+
+console.log(obj.employees[2].firstName+"   "+obj.employees[1].lastName);
+
