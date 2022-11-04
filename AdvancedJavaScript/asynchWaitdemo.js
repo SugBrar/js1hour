ac74e645b9c5a538b95ca84b9e6a2cba1dcdc98b
SugBrar <brar.sugreev@gmail.com>
2022-11-04 10:35:54 -0400

    await keyword

--- AdvancedJavaScript/asynchWaitdemo.js
+++ AdvancedJavaScript/asynchWaitdemo.js
@@ -1,21 +1,30 @@
-//Normal function
-function greet(){
-    return 'Hello'
+// //Normal function
+// function greet(){
+//     return 'Hello'
+// }
+// console.log(greet())
+
+// //async function
+// async function greet1(){
+//     return 'Hello'
+// }
+// console.log(greet1())
+
+// // resolve function
+// async function greet2(){
+//     return Promise.resolve('Hello')
+// }
+// //console.log(greet2())
+
+
+// greet2().then((value) => console.log(value))
+
+// await keyword 
+async function greet(){
+    let promise = new Promise((resolve,reject) => {
+        setTimeout(() => resolve('Hello'),1000)
+    });
+    let result = await promise; // wait until the promise resolves that is 1000 sec
+    console.log(result); // Hello
 }
-console.log(greet())
-
-//async function
-async function greet1(){
-    return 'Hello'
-}
-console.log(greet1())
-
-// resolve function
-async function greet2(){
-    return Promise.resolve('Hello')
-}
-//console.log(greet2())
-
-
-greet2().then((value) => console.log(value))
-
+greet()
